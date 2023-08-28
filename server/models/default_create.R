27a22c5cc3b915367a4ed97f60a4cbc7d1780708
Tilen Rupar <tilen.rupar@gmail.com>
2023-08-28 22:47:49 +0200

    Experimenting with keras

--- server/models/default_create.R
+++ server/models/default_create.R
@@ -12,10 +12,15 @@ query <- "SELECT datetime(open_time / 1000, 'unixepoch') AS open_time,
                  close, 
                  volume
           FROM klines
-          WHERE symbol = 'BTCUSDT'
-          ORDER BY open_time ASC;"
+          WHERE symbol = 'BTCUSDT';"
 data <- DBI::dbGetQuery(conn, query)
 
+# Get fee information
+# query_fee <- "SELECT *
+#           FROM asset_ticker
+#           WHERE symbol = 'BTCUSDT';"
+# 
+# fee_data <- DBI::dbGetQuery(conn, query_fee)
 # Disconnect from the database
 DBI::dbDisconnect(conn)
 
@@ -25,9 +30,6 @@ rownames(data) <- as.POSIXct(data$open_time)
 candles_df <- data[complete.cases(data), ]
 # Convert 'open_time' from milliseconds since the epoch to a date-time object
 # data$open_time <- as.POSIXct(data$open_time / 1000, origin="1970-01-01", tz="UTC")
-
-# Create an xts object for technical analysis (TTR lib)
-# candles_df <- as.xts(data) |> suppressWarnings()
 
 source(paste0(here::here(), "/models/functions/optimal_trading_signal.R"))
 source(paste0(here::here(), "/models/functions/add_ta.R"))
@@ -39,7 +41,7 @@ one_eight_the_candles <- round(quarter_the_candles/2)
 # Find the target (optimal signal)
 optimal_signal_params <- optimal_trading_signal(
   candles_df, 
-  max_holding_period = quarter_the_candles) 
+  max_holding_period = one_eight_the_candles) 
 
 signal <- optimal_signal_params$signals
 
@@ -56,9 +58,21 @@ how_many_ommited <- 1:sum(!complete.cases(cbind(signal, tech_ind)))
 
 ####  normalization of the tech_ind dataframe  ####
 
+
+# MinMax normalization function
+min_max_normalize <- function(x) {
+  if (any(!is.na(x))) {
+    (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
+  } else {
+    x
+  }
+}
+
+# Applying MinMax normalization after removing NAs
 tech_ind_normal <- as.data.frame(
-  apply(tech_ind, 2, scale)
-) 
+  apply(tech_ind, 2, min_max_normalize)
+)
+
 
 signal_str <- ifelse(signal == 1, "buy",
                      ifelse(signal == 0, "hold",
@@ -74,29 +88,106 @@ class.ind <- function(cl)
   as.data.frame(x)
 }
 
+
+# Convert to zero-based categorical values
+signal_zero_based <- signal + 1
+
 # create a modelling data frame
-signal_with_TA <- cbind(class.ind(signal_str), 
+signal_with_TA <- cbind(keras::to_categorical(signal_zero_based), 
                         tech_ind_normal)[-how_many_ommited,]
+y_labs <-  c("buy",  "hold", "sell")
+colnames(signal_with_TA)[1:3] <- y_labs
 
 # create a train set index for model training
 train_index <- 1:round(0.7*(nrow(signal_with_TA)))
 train <- signal_with_TA[train_index,]
 
-y_labs <- c("buy", "hold", "sell")
-x_train <- train[, !colnames(train) %in% y_labs]
-y_train <-  train[, colnames(train) %in% y_labs]
+x_train <- train[, !colnames(train) %in% y_labs & !colnames(train) %in% "signal_str"] |>
+  (\(x) keras::array_reshape(x, dim = dim(x)))()
 
+if(any(colnames(train) %in% y_labs)){
+  y_train <- train[, colnames(train) %in% y_labs] |>
+    (\(x) keras::array_reshape(x, dim = dim(x)))()
+  
+} else {
+  y_train <- data.frame(signal_str = train[,"signal_str"]) |>
+    (\(x) keras::array_reshape(x, dim = dim(x)))()
+}
 
 test <- signal_with_TA[-train_index,]
-x_test <- test[, !colnames(test) %in% y_labs]
-y_test <- test[, colnames(test) %in% y_labs]
+x_test <- test[, !colnames(test) %in% y_labs & !colnames(test) %in% "signal_str"] |>
+  (\(x) keras::array_reshape(x, dim = dim(x)))()
 
-##### Neural Net ###################################
-formula_str <- paste(
-  paste(colnames(y_train), collapse = " + "), 
-  "~", 
-  paste(colnames(x_train), collapse = " + ")
+if(any(colnames(test) %in% y_labs)){
+  y_test <- test[, colnames(test) %in% y_labs] |>
+    (\(x) keras::array_reshape(x, dim = dim(x)))()
+} else {
+  y_test <- data.frame(signal_str  = test[, "signal_str"]) |>
+    (\(x) keras::array_reshape(x, dim = dim(x)))()
+}
+
+#### KERAS NEURAL NET #####
+
+#clear keras session
+keras::k_clear_session()
+keras_nn_model <- keras::keras_model_sequential(input_shape = dim(x_train)[2]) |>
+
+  keras::layer_dense(dim(x_train)[2], activation = FLAGS$activation1) |>
+  keras::layer_dense(dim(x_train)[2]*2, activation = FLAGS$activation2, 
+                     kernel_regularizer = regularizer_l2(l = 0.001)) |>
+  keras::layer_dense(ncol(y_train), activation = "sigmoid")
+
+summary(keras_nn_model)
+
+# Calculate class weights manually based on class distribution
+class_weights_named <-  1 - as.numeric(prop.table(table(signal_str)))
+
+# Compile the model
+keras_nn_model |> keras::compile(
+  loss = 'categorical_crossentropy',
+  optimizer = keras::optimizer_rmsprop(learning_rate = 0.01),
+  metrics = c('mse'),
+  loss_weights = class_weights_named  # Specify class weights
 )
+
+# Define early stopping callback
+early_stopping <- keras::callback_early_stopping(
+  monitor = "loss",    # Metric to monitor (usually validation loss)
+  patience = 10,            # Number of epochs with no improvement before stopping
+  restore_best_weights = TRUE  # Restore model weights from the epoch with the best value
+)
+
+# Fit the model
+history <- keras_nn_model |> 
+  keras::fit(
+  x = x_train,
+  y = y_train,
+  epochs = 30,              # Adjust the number of epochs
+  batch_size = 128,          # Adjust the batch size
+  validation_split = 0.02,    # Optional: Validation split if you want to monitor validation loss
+  callbacks = list(early_stopping)  # Include the early stopping callback
+)
+
+keras_nn_model |>
+  keras::evaluate(as.matrix(x_test), as.matrix(y_test), batch_size = 32)
+
+predicted_probs <- keras_nn_model |> 
+  predict(as.matrix(x_test)) 
+
+predicted_classes <- apply(predicted_probs, 1, which.max)
+
+nnet_output <- data.frame(
+  prediction = y_labs[predicted_classes])
+
+table(nnet_output)
+
+######### Using neuralnet package #########################
+##### Neural Net ###################################
+# formula_str <- paste(
+#   paste(names(y_train), collapse = " + "), 
+#   "~", 
+#   paste(colnames(x_train), collapse = " + ")
+# )
 
 #Mutlithreading:
 # suppressWarnings(
@@ -107,27 +198,38 @@ formula_str <- paste(
 # train_h2o <- h2o::as.h2o(train)
 
 # Without multithreading (parallel processing)
-train_h2o <- train
+# train_h2o <- train
+# 
+# class_weights <- ifelse(
+#   y_train$signal_str %in% c("buy"), 1, 2)
 
-nnet_model <- neuralnet::neuralnet(
-  formula_str,
-  train_h2o, 
-  hidden = c(length(x_train)*2, length(x_train)), # 2 hidden layers
-  err.fct = "sse", #cross-entropy 'ce', 
-  linear.output = FALSE,  # Use softmax activation if FALSE                       
-  lifesign = 'full', # change this to 'none', for no logging
-  rep = 1, #number of repetitions for the neural network’s training
-  algorithm = "rprop+",
-  stepmax = 100000) # Boost this for more complex nnet
-
-source(paste0(here::here(), "/models/functions/predict_nnet.R"))
-
-nnet_output <- predict_nnet(nnet_model, test)
-
-# for development: confusion matrix - beyond some accuracy, do not save the model
-
-# Save the trained model to a file-
-saveRDS(nnet_model, "models/prediction_model.rds")
+# # Define custom loss function
+# custom_loss <- function(y_true, y_pred) {
+#   weights <- class_weights[y_true]  # Get class weights for the true labels
+#     weighted_losses <-  -weights * log(y_pred)  # Weighted cross-entropy loss
+#   sum(weighted_losses)
+# }
+# 
+# nnet_model <- neuralnet::neuralnet(
+#   formula_str,
+#   train_h2o, 
+#   hidden = c(length(x_train)*2, length(x_train)), # 2 hidden layers
+#   err.fct = "ce", 
+#   linear.output = FALSE,  # Use softmax activation if FALSE 
+#   learningrate = 0.01,
+#   lifesign = 'full', # change this to 'none', for no logging
+#   rep = 1, #number of repetitions for the neural network’s training
+#   algorithm = "rprop+",
+#   stepmax = 100000) # Boost this for more complex nnet
+# 
+# source(paste0(here::here(), "/models/functions/predict_nnet.R"))
+# 
+# nnet_output <- predict_nnet(nnet_model, test)
+# 
+# # for development: confusion matrix - beyond some accuracy, do not save the model
+# 
+# # Save the trained model to a file-
+# saveRDS(nnet_model, "models/prediction_model.rds")
 
 
 # if you are predicting test set:
@@ -142,7 +244,7 @@ plot_trading_signal <- function(ohlc_data, signals, buy = TRUE, sell = TRUE, tes
   # candle_hour_minute <- format(as.POSIXct(ohlc_data$open_time),"%H:%M")
   df <- data.frame(plot_time = as.POSIXct(ohlc_data$open_time),
                    plot_price = ohlc_data$close, 
-                   plot_signal = c(0,signals$signals))
+                   plot_signal = signals$signals) # c(0,signals$signals))
   df$plot_signal[-train_index] <- NA
   
   df <- merge(df, test_predictions, by= "plot_time", all = TRUE)
@@ -165,7 +267,12 @@ plot_trading_signal <- function(ohlc_data, signals, buy = TRUE, sell = TRUE, tes
                               labels = scales::date_format("%Y-%m-%d %H:%M")) +
     ggplot2::geom_vline(xintercept = df[max(train_index), 'plot_time'], color = "red") +
     ggplot2::theme_dark() +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 0.1))
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 0.1, color = "white"),
+                   axis.text.y = ggplot2::element_text(color = "white"),
+                   plot.background =  ggplot2::element_rect(fill =  rgb(20/255, 30/255, 38/255)),
+                   plot.title = ggplot2::element_text(color = "white"),
+                   plot.subtitle = ggplot2::element_text(color = "white"), 
+                   axis.title = ggplot2::element_text(color = "white"))
 }
 
 historical_signal_plot <- plot_trading_signal(
@@ -178,6 +285,9 @@ suppressMessages(
   ggplot2::ggsave(
     filename = "static/historical_trading_signals_model.svg", 
     plot = historical_signal_plot, 
+    width = 10.68,  # Specify the desired width in inches
+    height = 5, 
+    limitsize = FALSE,
     device = "svg")
 )
 
